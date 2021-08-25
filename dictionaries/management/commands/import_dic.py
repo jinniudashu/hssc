509a8224c07195c1d726d697a41f0e74fb865b23
jinniudashu <jinniudashu@gmail.com>
2021-08-25 21:23:01 +0800

    Added import_dic.py

--- dictionaries/management/commands/import_dic.py
+++ dictionaries/management/commands/import_dic.py
@@ -4,46 +4,59 @@ import requests
 from dictionaries.models import *
 
 
-# python manage.py import_icpc
+# python manage.py import_dic
 
 class Command(BaseCommand):
-    help = '从需求系统的字典库dictionary_data中导入字典编码，参数：--dic, all-导入所有字典，dicname-导入单个字典'
+    help = '从需求系统的dictionary中导入字典编码（单个字典100条以内），参数：--dic, all-导入所有字典，dicname-导入单个字典。'
 
     # 只能导入100条以内的记录
-    def run_import(self, name):
+    def run_import(self, dic):
+        id = dic['id']
+        name = dic['name']
+        label = dic['label']
         print(f'正在导入 {name} ...')
 
         # GraphQl style ===================
-        query = """query {
-            dictionaryData {
-                id
-                value
-                score
-                dictionary {
+        query = """
+            query getDicData($id: ID!){
+                dictionary(id: $id){
+                    id
                     name
+                    label
+                    dictionary_data{
+                        value
+                        score
+                    }
                 }
-            }
-        }"""
+            }              
+        """
+        variables = {'id': id}
         url = 'https://hssc-rcms.herokuapp.com/graphql'
-        res = requests.post(url, json={'query': query})
-        dics = res.json()['data']['dictionaryData']
+        res = requests.post(url, json={'query': query, 'variables': variables})
 
-        # 把name 转为 model class
-        dic_model= globals()[name.capitalize()]
-        i = 0
-        for dic in dics:
-            i += 1
-            try:
-                dic_model.objects.create(
-                    name = dic['value'],
-                    score = dic['score'],
-                )
-            except:
-                print (f'Error:{i}')
-            else:
-                print (dic['id'], dic['value'], dic['dictionary']['name'])
+        print(res.text)
+        data = res.json()['data']['dictionary']
 
-        print(f'{name}: 成功导入{i}条记录')
+        # 如果data['dictionary_data']内有记录，导入
+        if len(data['dictionary_data']) > 0:
+            # 把name 转为 model class
+            dic_model= globals()[name.capitalize()]
+            i = 0
+            for dic in data['dictionary_data']:
+                i += 1
+                try:
+                    dic_model.objects.create(
+                        name = dic['value'],
+                        score = dic['score'],
+                    )
+                except:
+                    print (f'Error:{i}')
+                else:
+                    print (dic['value'], dic['score'])
+
+            print(f'{name}: 成功导入{i}条记录')
+        else:
+            print('空字典')
 
 
     def add_arguments(self, parser):
@@ -67,7 +80,7 @@ class Command(BaseCommand):
                     dic_list = res.json()
                     #逐一导入字典
                     for dic in dic_list:
-                        self.run_import(dic['name'].strip().lower())
+                        self.run_import(dic)
                 else:
                     print('all-没有找到可以导入的字典！')
 
@@ -76,6 +89,7 @@ class Command(BaseCommand):
                 # 验证字典表是否存在
                 res = requests.get(f'{dic_list_url}?name={dic}')
                 if len(res.json()) == 1:
+                    dic = res.json()[0]
                     self.run_import(dic)
                 else:
                     print(f'没有这个字典{dic}')
