c3531829b76e8afeb46d41538dfab4517ba952fd
JiangNing <jinniudashu@gmail.com>
2022-02-13 18:33:50 +0800

    Added index_customer.html Modified CBV Index_staff to FBV index_staff()

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import Index_staff, htmx_test
+from .views import index_staff, htmx_test
 
 # app_name = 'forms'
 
 urlpatterns = [	
 	path('', htmx_test, name='htmx_test'),
-	path('index_staff/', Index_staff.as_view(), name='index_staff'),
+	path('index_staff/', index_staff, name='index_staff'),
 ]
