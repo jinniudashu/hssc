b337858d7a87fc8f0bd3d870d0b56ecca1088b4e
jinniudashu <jinniudashu@gmail.com>
2022-05-07 23:10:36 +0800

    优化客户登录url

--- hssc/views.py
+++ hssc/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, redirect
 def index(request):
     if request.user.is_authenticated:
         if request.user.is_staff:
-            return redirect('index_staff')
+            return redirect('/clinic/')
         else:
-            return redirect('index_customer')
+            return redirect('/core/index_customer/')
     else:
         return redirect('/accounts/login/')
