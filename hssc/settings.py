442332fd9f246479517d164df2b775ac6be41dc4
jinniudashu <jinniudashu@gmail.com>
2022-06-04 23:38:53 +0800

    Deleted CRISPY in settings.py

--- hssc/settings.py
+++ hssc/settings.py
@@ -178,8 +178,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
