b4a0d0287c175b35c354fa9d5de83e0fb3658b51
jinniudashu <jinniudashu@gmail.com>
2022-06-04 23:47:59 +0800

    Updated asgi.py

--- hssc/asgi.py
+++ hssc/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hssc.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from core.routing import ws_urlpatterns
